e7192f5a14faf7f8100bd6e7a7a5214e76122861
techman104 <87010449+techman104@users.noreply.github.com>
2021-08-05 18:37:04 +0900

    issueの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :set_prototype, only: [:index, :create, :new]
-  before_action :redirect_root, except: :index
+  before_action :authenticate_user!, only: [:show, :index, :new, :edit]
+  before_action :move_to_index, except: [:index, :show, :new]
   
   def index
     @prototypes = Prototype.includes(:user)
@@ -13,7 +13,7 @@ class PrototypesController < ApplicationController
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
-    @comments = @prototype.comments
+    @comments = @prototype.comments.includes(:user)
   end  
 
   def edit
@@ -53,16 +53,13 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
   
-  def set_prototype
-  end
-
-  def redirect_root
-    redirect_to root_path unless user_signed_in?
-  end
-  
-  # def move_to_index
-  #   unless user_signed_in?
-  #     redirect_to action: :index
-  #   end
+  # def set_prototype
   # end
+  
+  def move_to_index
+    prototype = Prototype.find(params[:id])
+    if prototype.user_id != current_user.id
+      redirect_to action: :index  
+    end
+  end  
 end
